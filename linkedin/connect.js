56ea8ec5ee9d4746bd983a009f3fbe35f4566375
Lincoln W Daniel <lwdthe1@gmail.com>
2016-09-01 01:08:38 -0400

    Create connect.js

---
+++ linkedin/connect.js
@@ -0,0 +1,38 @@
+var jfjkfhdConnectAttemps = 0;
+var jfjkfhdTotalConnected = 0;
+var restSecs = 5;
+function jkhasdScrollPage() {
+	var connectedThisTime = 0;
+	
+	window.scrollBy(0,-15); // scroll up first
+	//then scroll down because linkedin requires this
+	window.scrollTo(0,document.body.scrollHeight); // horizontal and vertical scroll increments
+	
+	
+	var linkedInConnectButtons = document.getElementsByClassName('bt-request-buffed buffed-blue-bkg-1');
+	for(var i = 0; i < linkedInConnectButtons.length; i++) {
+		var connectButton = linkedInConnectButtons[i];
+		var connectButtonInnerHTML = connectButton.innerHTML;
+	
+		if(connectButton.disabled == false) {
+			//console.log(i + 'y| ' + connectButtonInnerHTML);
+			connectButton.click();
+			connectButton.innerHTML = "CONNECTED";
+			//connectButton.disabled = true;
+			++jfjkfhdTotalConnected;
+			++connectedThisTime;
+		}
+	}
+	
+	if(connectedThisTime > 0) console.log(++jfjkfhdConnectAttemps + " | tf:" + jfjkfhdTotalConnected);
+	if(jfjkfhdConnectAttemps <= 80) scrolldelay = setTimeout('jkhasdScrollPage()', restSecs * 1000); // scrolls every 100 milliseconds
+	else {
+		console.log("Connected " + jfjkfhdTotalConnected);
+		console.log("Restarting in an hour ...");
+		jfjkfhdConnectAttemps = 0;
+		jfjkfhdTotalConnected = 0;
+		setTimeout('jkhasdScrollPage()', 30 * 60 * 1000);
+	}
+}
+//call the method
+jkhasdScrollPage();
