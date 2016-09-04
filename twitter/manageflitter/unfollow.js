add4516020ded8213e23cc98831d5dc140c7c162
Lincoln W Daniel <lwdthe1@gmail.com>
2016-09-04 18:08:25 -0400

    Create unfollow.js

---
+++ twitter/manageflitter/unfollow.js
@@ -0,0 +1,36 @@
+var jfjkfhdFollowAttemps = 0;
+var jfjkfhdTotalFollowed = 0;
+var restSecs = 2;
+function jkhasdScrollPage() {
+	var followedThisTime = 0;
+
+	window.scrollBy(0,50); // horizontal and vertical scroll increments
+	
+	var twitterFollowButtons = document.getElementsByClassName('process mode-basic Unfollow');
+	//alert(twitterFollowButtons.length);
+	for(var i = 0; i < twitterFollowButtons.length; i++) {
+		var followButton = twitterFollowButtons[i];
+
+			if(followButton.disabled == false) {
+    			//console.log(i + 'y');
+    			followButton.click();
+
+    			followButton.disabled = true;
+    			++jfjkfhdTotalFollowed;
+    			++followedThisTime;
+    		}
+
+	}
+	
+	if(followedThisTime > 0) console.log(++jfjkfhdFollowAttemps + " | tf:" + jfjkfhdTotalFollowed);
+	if(jfjkfhdFollowAttemps <= 80) scrolldelay = setTimeout('jkhasdScrollPage()', restSecs * 1000); // scrolls every 100 milliseconds
+	else {
+		console.log("Followed " + jfjkfhdTotalFollowed);
+		console.log("Restarting in an hour ...");
+		jfjkfhdFollowAttemps = 0;
+		jfjkfhdTotalFollowed = 0;
+		setTimeout('jkhasdScrollPage()', 30 * 60 * 1000);
+	}
+}
+//call the method
+jkhasdScrollPage();
