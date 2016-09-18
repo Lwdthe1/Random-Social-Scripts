4575cc9f8b325ef64849eab17161ceeb7cfed9cf
Lincoln W Daniel <lwdthe1@gmail.com>
2016-09-18 18:33:27 -0400

    Create getAllEditors.js

---
+++ supermeditor/getAllEditors.js
@@ -0,0 +1,8 @@
+var editorIds = ""
+var uniqueEditorIds = $(".link.clean-text.ng-binding");
+for(var i = 0; i < editors.length; i++) {
+  var current = editors[i].innerHTML.trim()
+  if(uniqueEditorIds.indexOf(current) > -1) continue
+  uniqueEditorIds += current + " "
+}
+console.log(uniqueEditorIds)
