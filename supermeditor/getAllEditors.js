805d806928ae4652a399e3131eb44f985ae4ad31
Lincoln W Daniel <lwdthe1@gmail.com>
2016-12-18 05:24:06 -0500

    Update getAllEditors.js

--- supermeditor/getAllEditors.js
+++ supermeditor/getAllEditors.js
@@ -1,8 +1,8 @@
 var uniqueEditorIds = ""
-var editors = $(".link.clean-text.ng-binding");
+var editors = $(".link.u-colorLightGrey.u-fontWeight400.u-fontSize12px.ng-binding");
 for(var i = 0; i < editors.length; i++) {
   var current = editors[i].innerHTML.trim()
   if(uniqueEditorIds.indexOf(current) > -1) continue
-  uniqueEditorIds += current + " "
+  uniqueEditorIds += "@" +current + " "
 }
 console.log(uniqueEditorIds)
