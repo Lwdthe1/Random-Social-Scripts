dd116c750f5bf0c8b81dd64da056401bf9711c3f
Lincoln W Daniel <lwdthe1@gmail.com>
2017-02-23 22:32:17 -0500

    Update getAllEditors.js

--- supermeditor/getAllEditors.js
+++ supermeditor/getAllEditors.js
@@ -1,5 +1,5 @@
 var uniqueEditorIds = ""
-var editors = $(".link.u-colorLightGrey.u-fontWeight400.u-fontSize12px.ng-binding");
+var editors = $(".pub-contributor-link.link.u-colorLightGrey.u-fontWeight400.u-fontSize12px");
 for(var i = 0; i < editors.length; i++) {
   var current = editors[i].innerHTML.trim()
   if(uniqueEditorIds.indexOf(current) > -1) continue
