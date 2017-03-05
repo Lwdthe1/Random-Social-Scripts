690d2a6544e3b3b521a409348dd87ec2780a2c8c
Lincoln W Daniel <lwdthe1@gmail.com>
2017-03-05 18:48:02 -0500

    Create like_feed.js

---
+++ instagram/like_feed.js
@@ -0,0 +1,35 @@
+var jfjkfhdFollowAttemps = 0;
+var jfjkfhdTotalFollowed = 0;
+var restSecs = 2;
+function jkhasdScrollPage() {
+	var followedThisTime = 0;
+
+	window.scrollBy(0,50); // horizontal and vertical scroll increments
+	
+	var twitterFollowButtons = document.getElementsByClassName('_ebwb5 _1tv0k');
+	//alert(twitterFollowButtons.length);
+	for(var i = 0; i < twitterFollowButtons.length; i++) {
+		var followButton = twitterFollowButtons[i];
+		var followSpan = followButton.innerHTML
+		if(followSpan != "Follow") { continue; }
+        if(followButton.disabled == false) {
+            followButton.click();
+            followSpan[0].innerHTML = "FOLLOWED";
+            followButton.disabled = true;
+            ++jfjkfhdTotalFollowed;
+            ++followedThisTime;
+        }
+	}
+	
+	if(followedThisTime > 0) console.log(++jfjkfhdFollowAttemps + " | tf:" + jfjkfhdTotalFollowed);
+	if(jfjkfhdFollowAttemps <= 80) scrolldelay = setTimeout('jkhasdScrollPage()', restSecs * 1000); // scrolls every 100 milliseconds
+	else {
+		console.log("Followed " + jfjkfhdTotalFollowed);
+		console.log("Restarting in an hour ...");
+		jfjkfhdFollowAttemps = 0;
+		jfjkfhdTotalFollowed = 0;
+		setTimeout('jkhasdScrollPage()', 30 * 60 * 1000);
+	}
+}
+//call the method
+jkhasdScrollPage();
