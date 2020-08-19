a3281298096ebec12a51ba269edd8b8c46ed4add
Lincoln W Daniel <lwdthe1@gmail.com>
2020-08-19 19:29:44 -0400

    Update partner_program_earnings_analysis.js

--- medium/partner_program_earnings_analysis.js
+++ medium/partner_program_earnings_analysis.js
@@ -1,4 +1,38 @@
 function runEarningsAnalysis() {
+  const predicates = [
+    (price) => 0.01,
+    (price) => [0.02, 0.50],
+    (price) => [0.51, 1.00],
+    (price) => [1.01, 500],
+    (price) => [5.01, 10.00],
+  ]
+  
+  // Shape the groups
+  const groups = predicates.reduce((arr, predicate) => {
+    const group = {
+      predicate,
+      prices: [],
+      numPrices: 0,
+      maybeAddPrice(price) {
+        if (!predicate(price)) {
+          return
+        }
+        this._addPrice(price)
+      },
+      getPriceRange() {
+        const sorted = group.prices.sort()
+        return {min: sorted[0], max: sorted[sorted.length - 1]}
+      },
+      _addPrice(price) {
+        this.prices.push(price)
+        this.numPrices = group.prices.length
+      },
+    }
+    
+    arr.push(group)
+    return arr
+  }, [])
+  
   const stories = Array.from(document.querySelectorAll('.js-postAmount')).map((node) => {
       return {price: parseFloat(node.innerText.replace('$', ''))}
   })
@@ -10,40 +44,11 @@ function runEarningsAnalysis() {
     group.numPrices++
   }
   
-  const groupMap = {
-    // These keys must all 
-    // start with "e", 
-    // the two numbers should be separated by "T",
-    // and the numbers should represent cents ($)
-    e001: {}, e002To050: {}, e051To100: {}, e101To500: {}, e501To1000: {}, e1001Up: {}
-  }
-  
-  // Shape the groups
-  Object.values(groupMap).forEach((group) => {
-    group.prices = []
-    group.numPrices = 0
-    
-    group.addPrice = (price) => {
-      group.prices.push(price)
-      group.numPrices = group.prices.length
-    }
-    
-    group.getPriceRange = () => {
-      const sorted = group.prices.sort()
-      return {min: sorted[0], max: sorted[sorted.length - 1]}
-    }
-  })
-  
   stories.forEach(({price}) => {
-    if (price === 0.01) groupMap.e001.addPrice(price)
-    if (price >= 0.02 && price <= 0.5) groupMap.e002To050.addPrice(price)
-    if (price >= 0.51 && price <= 1) groupMap.e051To100.addPrice(price) 
-    if (price >= 1.01 && price <= 5) groupMap.e101To500.addPrice(price) 
-    if (price >= 5.01 && price <= 10) groupMap.e501To1000.addPrice(price)
-    if (price >= 10.01) groupMap.e1001Up.addPrice(price)
+      groups.forEach((group) => group.maybeAddPrice(price))
   })
   
-  Object.values(groupMap).forEach((group) => {
+groups.forEach((group) => {
     if (!group.numPrices) {
       return
     }
@@ -53,7 +58,7 @@ function runEarningsAnalysis() {
     group.report = `${group.numPrices} of my stories earned ${rangeDisplay}.`
   })
   
-  const totalReportData = Object.values(groupMap).reduce((data, group) => {
+  const totalReportData = groups.reduce((data, group) => {
     const {min, max} = group.getPriceRange()
     if (min === undefined) {
       return
@@ -73,7 +78,7 @@ function runEarningsAnalysis() {
   }, {numPrices: 0, minPrice: 0, maxPrice: 0})
   
   const report = `I had ${totalReportData.numPrices} stories that earned me between $${totalReportData.minPrice} and $${totalReportData.maxPrice}. 
-    ${Object.values(groupMap).map(({report}) => report).join('\n\n   ')}`
+    ${groups.map(({report}) => report).join('\n\n   ')}`
   
   console.log(
     `%c✨💸 My earnings report for ${periodDisplay}: \n%c${report}`, 
@@ -81,7 +86,7 @@ function runEarningsAnalysis() {
     'color:#333; font-size:14px; font-weight: 700'
   )
 
-  return {groupMap, report}
+  return {groups, report}
 }
 
 console.log(runEarningsAnalysis())
